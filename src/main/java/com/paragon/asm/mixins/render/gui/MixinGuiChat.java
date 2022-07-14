b717bcef2be9020053a8ea1e101d9aadf3235777
Wolfsurge <iantopettitt123@gmail.com>
2022-07-14 20:35:58 +0100

    Fix Mixin issue (possibly)

--- src/main/java/com/paragon/asm/mixins/render/gui/MixinGuiChat.java
+++ src/main/java/com/paragon/asm/mixins/render/gui/MixinGuiChat.java
@@ -2,6 +2,7 @@ package com.paragon.asm.mixins.render.gui;
 
 import com.paragon.Paragon;
 import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.GuiTextField;
 import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,12 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinGuiChat {
 
     @Shadow
-    protected abstract void setText(String newChatText, boolean shouldOverwrite);
+    protected GuiTextField inputField;
 
     @Inject(method = "drawScreen", at = @At("HEAD"))
     public void onDrawScreen(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
-            this.setText(Paragon.INSTANCE.getCommandManager().getLastCommand(), true);
+            this.inputField.setText(Paragon.INSTANCE.getCommandManager().getLastCommand());
         }
     }
 
