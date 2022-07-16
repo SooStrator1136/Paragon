4882be6f4622543a73a1fb3fb0045243e6de3c5b
Wolfsurge <iantopettitt123@gmail.com>
2022-07-16 23:26:56 +0100

    CustomText HUD module

--- src/main/java/com/paragon/client/managers/ModuleManager.kt
+++ src/main/java/com/paragon/client/managers/ModuleManager.kt
@@ -113,6 +113,7 @@ class ModuleManager {
             CombatInfo(),
             Coordinates(),
             Crystals(),
+            CustomText,
             Direction(),
             FPS(),
             HUD(),
---
+++ src/main/java/com/paragon/client/systems/module/hud/impl/CustomText.kt
@@ -0,0 +1,30 @@
+package com.paragon.client.systems.module.hud.impl
+
+import com.paragon.api.setting.Setting
+import com.paragon.client.systems.module.hud.HUDModule
+import java.awt.Color
+
+/**
+ * @author Wolfsurge
+ */
+object CustomText : HUDModule("CustomText", "Display custom text of your choice!") {
+
+    private val text = Setting("Text", "Paragon on top!")
+        .setDescription("The text to display")
+
+    private val textColour = Setting("TextColour", Color.WHITE)
+        .setDescription("The colour of the text")
+
+    override fun render() {
+        renderText(text.value, x, y, textColour.value.rgb)
+    }
+
+    override fun getWidth(): Float {
+        return getStringWidth(text.value)
+    }
+
+    override fun getHeight(): Float {
+        return fontHeight
+    }
+
+}
