31dfa8b08f7d68ba9af938282010083af8c6968e
Wolfsurge <iantopettitt123@gmail.com>
2022-08-18 22:40:43 +0100

    Fix Changelog window scrolling

--- src/main/java/com/paragon/client/ui/configuration/windows/impl/ChangelogWindow.kt
+++ src/main/java/com/paragon/client/ui/configuration/windows/impl/ChangelogWindow.kt
@@ -7,8 +7,10 @@ import com.paragon.client.systems.module.impl.client.ClickGUI
 import com.paragon.client.systems.module.impl.client.Colours
 import com.paragon.client.ui.configuration.windows.Window
 import com.paragon.client.ui.util.Click
+import net.minecraft.util.math.MathHelper
 import org.apache.commons.io.IOUtils
 import java.nio.charset.StandardCharsets
+import kotlin.math.max
 
 /**
  * @author Surge
@@ -17,35 +19,52 @@ import java.nio.charset.StandardCharsets
 class ChangelogWindow(x: Float, y: Float, width: Float, height: Float, grabbableHeight: Float) : Window(x, y, width, height, grabbableHeight) {
 
     private val changelog: ArrayList<String> = arrayListOf()
+    private var scroll = 0f
 
     init {
         val inputStream = javaClass.getResourceAsStream("/assets/paragon/changelog.txt")
         changelog.addAll(IOUtils.toString(inputStream, StandardCharsets.UTF_8).split(System.lineSeparator()))
     }
 
+    override fun scroll(mouseX: Int, mouseY: Int, mouseDelta: Int): Boolean {
+        if (mouseDelta != 0 && mouseX.toFloat() in x..x + width && mouseY.toFloat() in y..y + height) {
+            scroll += FontUtil.getHeight() * if (mouseDelta > 0) 1 else -1
+
+            return true
+        }
+
+        return false
+    }
+
     override fun draw(mouseX: Int, mouseY: Int, mouseDelta: Int) {
         super.draw(mouseX, mouseY, mouseDelta)
 
-        RenderUtil.pushScissor(x.toDouble(), y.toDouble(), width.toDouble() * openAnimation.getAnimationFactor(), (height.toDouble() + 1) * openAnimation.getAnimationFactor())
+        scroll = MathHelper.clamp(scroll.toDouble(), -max(0.0, (height - 208).toDouble()), 0.0).toFloat()
 
-        RenderUtil.drawRect(x, y, width, height, 0x90000000.toInt())
+        RenderUtil.drawRect(x, y, (width * openAnimation.getAnimationFactor()).toFloat(), (height * openAnimation.getAnimationFactor()).toFloat(), 0x90000000.toInt())
 
         if (ClickGUI.blur.value) {
             BlurUtil.blur(x.toInt(), y.toInt(), (width * openAnimation.getAnimationFactor()).toInt(), (height * openAnimation.getAnimationFactor()).toInt(), ClickGUI.intensity.value.toInt())
         }
 
+        RenderUtil.pushScissor(x.toDouble(), y.toDouble(), width * openAnimation.getAnimationFactor(), 16 * openAnimation.getAnimationFactor())
+
         RenderUtil.drawRect(x, y, width * openAnimation.getAnimationFactor().toFloat(), grabbableHeight, Colours.mainColour.value.rgb)
         FontUtil.drawStringWithShadow("Changelog", x + 3, y + 4, -1)
 
+        RenderUtil.drawRect((x + ((width - 16f) * openAnimation.getAnimationFactor())).toFloat(), y, 16f, grabbableHeight, 0x90000000.toInt())
+        FontUtil.defaultFont.drawStringWithShadow("X", (x + width - 9f) - (FontUtil.defaultFont.getStringWidth("X") / 2f), y + 1.5f, -1)
+
+        RenderUtil.popScissor()
+
         RenderUtil.drawBorder(x + 0.5f, y + 0.5f, ((width - 1) * openAnimation.getAnimationFactor()).toFloat(), ((height - 1) * openAnimation.getAnimationFactor()).toFloat(), 0.5f, Colours.mainColour.value.rgb)
 
-        RenderUtil.drawRect(x + width - 16f, y, 16f, grabbableHeight, 0x90000000.toInt())
-        FontUtil.defaultFont.drawStringWithShadow("X", (x + width - 9f) - (FontUtil.defaultFont.getStringWidth("X") / 2f), y + 1.5f, -1)
+        RenderUtil.pushScissor(x.toDouble(), y.toDouble() + 17, width.toDouble() * openAnimation.getAnimationFactor(), (height.toDouble() - 18) * openAnimation.getAnimationFactor())
 
         var offset = grabbableHeight + 5f
 
         changelog.forEach {
-            FontUtil.drawStringWithShadow(it, x + 5, y + offset, -1)
+            FontUtil.drawStringWithShadow(it, x + 5, y + offset + scroll, -1)
 
             offset += FontUtil.getHeight()
         }
