0b153aa300192fd05574b9a15b4340e42fe92feb
bush <101901339+therealbush@users.noreply.github.com>
2022-09-12 15:02:10 -0700

    😎

--- src/main/kotlin/com/paragon/MixinLoader.kt
+++ src/main/kotlin/com/paragon/MixinLoader.kt
@@ -20,7 +20,6 @@ class MixinLoader : IFMLLoadingPlugin {
     override fun getASMTransformerClass(): Array<String> = emptyArray()
     override fun getModContainerClass(): String? = null
     override fun getSetupClass(): String? = null
-    override fun injectData(data: Map<String, Any>) {}
+    override fun injectData(data: Map<String, Any>) = Unit
     override fun getAccessTransformerClass(): String? = null
-
 }
